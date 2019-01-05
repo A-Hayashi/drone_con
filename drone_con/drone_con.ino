7bec24d7ff0b81d5c1eefd13ffdb6d4b6aaf7d52
dracen <a.hayashi0113@gmail.com>
2019-01-05 17:08:17 +0900

    DAC動作確認済

--- drone_con/drone_con.ino
+++ drone_con/drone_con.ino
@@ -1,5 +1,9 @@
-#include "PS_PAD.h"
 #include "SPI.h"
+#include "PS_PAD.h"
+#include "MCP4922.h"
+
+#define MAX(a,b) ((a) > (b) ? a : b)
+#define MIN(a,b) ((a) < (b) ? a : b)
 
 #define A5 19
 #define A4 18
@@ -11,29 +15,72 @@
 #define PS2_SEL 10
 PS_PAD PAD(PS2_SEL);
 
+MCP4922 DAC1(11, 13, A2, A3); // (MOSI,SCK,CS,LDAC) define Connections for NANO_board,
+MCP4922 DAC2(11, 13, A4, A5); // (MOSI,SCK,CS,LDAC) define Connections for NANO_board,
+
 void setup() {
+  SPI.begin();
+  
   pinMode(PS2_SEL, OUTPUT);
   digitalWrite(PS2_SEL, HIGH);
-  PAD.init();
+//  PAD.init();
+  
   Serial.begin(9600);
 }
 
+void loop()
+{
+  float v = 0;
+  while (true) {
+    v += 0.1;
+    if (v > 5.0) v = 0;
+    DAC1.Set(calcDAC(v), calcDAC(5.0 - v));
+    DAC2.Set(calcDAC(5.0 - v), calcDAC(v));
+    delay(500);
 
-void loop() {
-  PAD.poll();
-  
-  int lx = PAD.read(PS_PAD::ANALOG_LX);
-  int ly = PAD.read(PS_PAD::ANALOG_LY);
-  int rx = PAD.read(PS_PAD::ANALOG_RX);
-  int ry = PAD.read(PS_PAD::ANALOG_RY);
-
-
-  Serial.print(lx);
-  Serial.print("\t");
-  Serial.print(ly);
-  Serial.print("\t");
-  Serial.print(rx);
-  Serial.print("\t");
-  Serial.print(ry);
-  Serial.println();
+    Serial.print("DAC1:");
+    Serial.print (getVoltage(analogRead(A0)));
+    Serial.print("  ");
+    Serial.print (getVoltage(analogRead(A1)));
+    Serial.print("\t");
+    Serial.print("DAC2:");
+    Serial.print (getVoltage(analogRead(A6)));
+    Serial.print("  ");
+    Serial.println (getVoltage(analogRead(A7)));
+  }
 }
+
+
+int calcDAC(float v)
+{
+  int val = (v * 4096) / 5.0;
+  val = MAX(val, 0);
+  val = MIN(val, 4095);
+  return val;
+}
+
+float getVoltage(int ad)
+{
+  float val = (ad * 5.0) / 1024;
+  return val;
+}
+
+
+//void loop() {
+//  PAD.poll();
+//
+//  int lx = PAD.read(PS_PAD::ANALOG_LX);
+//  int ly = PAD.read(PS_PAD::ANALOG_LY);
+//  int rx = PAD.read(PS_PAD::ANALOG_RX);
+//  int ry = PAD.read(PS_PAD::ANALOG_RY);
+//
+//
+//  Serial.print(lx);
+//  Serial.print("\t");
+//  Serial.print(ly);
+//  Serial.print("\t");
+//  Serial.print(rx);
+//  Serial.print("\t");
+//  Serial.print(ry);
+//  Serial.println();
+//}
